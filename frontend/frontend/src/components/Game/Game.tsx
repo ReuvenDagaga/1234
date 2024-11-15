0fa9a744694f98f1031afe8afcbc7103b145214d
Reuven <dagagareuven@gmail.com>
2024-11-15 13:55:02 +0200

    created basics rouers and installed react-router-dom

---
+++ frontend/frontend/src/components/Game/Game.tsx
@@ -0,0 +1,11 @@
+
+
+const Game = () => (
+    <div style={{ textAlign: 'center', marginTop: '10%' }}>
+      <h1>Game Screen</h1>
+      <p>Here the game will take place!</p>
+    </div>
+  );
+  
+  export default Game;
+  
